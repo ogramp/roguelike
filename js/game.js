7ea8de6550aa607cc77f26973db1efbfbced40fc
Margo Pruul <ogramp@gmail.com>
2016-08-02 13:10:21 +0300

    chagne the logic by witch the number of walls on level is calculated

--- js/game.js
+++ js/game.js
@@ -3,8 +3,11 @@ Game.prototype.preload = function() {
 	game.roguelike.levelNumber += 1;
 };
 Game.prototype.create = function() {
-	game.roguelike.level = new Level(game.roguelike.levelNumber, 42, 2);
+	var numberOfInnerWallsOnLevel = Math.ceil(((game.roguelike.columns-2)*(game.roguelike.rows-2))/3);
+	game.roguelike.level = new Level(game.roguelike.levelNumber, numberOfInnerWallsOnLevel, 2);
 	game.roguelike.player = game.add.existing(new Player(1, game.roguelike.rows));
+
+	console.log(numberOfInnerWallsOnLevel);
 
 	var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	spaceKey.onDown.add(this.startNewLevel, this);
