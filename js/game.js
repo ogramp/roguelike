cc863ef72420db89e2c55d04ad1ea0ae4acd5c7e
Margo Pruul <ogramp@gmail.com>
2016-08-02 13:02:20 +0300

    change the number of walls on level 1

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ Game.prototype.preload = function() {
 	game.roguelike.levelNumber += 1;
 };
 Game.prototype.create = function() {
-	game.roguelike.level = new Level(game.roguelike.levelNumber, 2, 2);
+	game.roguelike.level = new Level(game.roguelike.levelNumber, 42, 2);
 	game.roguelike.player = game.add.existing(new Player(1, game.roguelike.rows));
 
 	var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
