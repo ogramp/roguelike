abe3c9a6fcf7f655a96464b7a3a83f7b1bc2c6fb
Margo Pruul <margo.pruul@gmail.com>
2016-08-06 14:22:07 +0300

    change menu styles

--- js/style.js
+++ js/style.js
@@ -1,21 +1,21 @@
 var style;
 
 (function() {
-	var defaultColor = 'white';
-	var highlightColor = '#FEFFD5';
+	var defaultColor = '#9EA2A8';
+	var highlightColor = '#646464';
 
 	style = {
 	
 		header: {
-			font: 'bold 40pt Slackey',
+			font: 'bold 32pt Slackey',
 			fill: defaultColor,
 			align: 'center'
 		},
 		navitem: {
 			base: {
-				font: '30pt Slackey',
+				font: '16pt Slackey',
 				align: 'center',
-				strokeThickness: 4
+				strokeThickness: 2
 			},
 			default: {
 				fill: defaultColor,
@@ -23,11 +23,11 @@ var style;
 			},
 			hover: {
 				fill: highlightColor,
-				stroke: 'rgba(200, 200, 200, 0.5)'
+				stroke: 'rgba(100, 100, 100, 0.5)'
 			}
 		}
 
-	}
+	};
 
 	Object.assign(style.navitem.hover, style.navitem.base);
 	Object.assign(style.navitem.default, style.navitem.base);
